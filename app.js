d6b4d626583f696e2273a4b89a686ef3fb1d7114
Azeez Wahab <azeezwahab24@gmail.com>
2025-07-09 07:02:13 +0100

    serving static files

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
 
 const PORT = process.env.PORT;
+
+// file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // initializing express
 const app = express();
 
 // creating endpoints 
+// 1 setting static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
 
-
-});
+// });
 
 // listen to server 
 app.listen(PORT , () =>{
